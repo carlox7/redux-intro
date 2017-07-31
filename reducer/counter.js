4460d59ed606009f1013690e5f4afb87bcc4ec4e
Carlo Santos <carlox7@gmail.com>
2017-07-31 15:58:57 -0700

    created reducers

--- reducer/counter.js
+++ reducer/counter.js
@@ -1,3 +1,21 @@
 'use strict';
 
+//redux reducers both define the state, and the interactions to the state
 //function signature of a redux reducer (oldState, action) => newState
+
+//an action is object with two proptypes(type, payload)
+//it gets passed into a reducer and the reducer ca use the action to update the state
+let intialState = 0;
+
+reducer = (state=0, action) => {
+  let {type, payload} = action
+
+  switch(type){
+    case 'INC':
+      return state + (payload ? payload : 1)
+    case 'DEC':
+      return state - (payload ? payload : 1)
+    default:
+      return state
+  }
+}
